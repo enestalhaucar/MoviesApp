6e087dc336c3ae1e4ec579128d0986c7fca147a6
Enes Talha Uçar <talhauenes@gmail.com>
2023-11-23 21:33:59 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Enes Talha Uçar  on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
